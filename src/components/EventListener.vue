43e2c27dd08a3adc1bdb8a45ef68c126e3110f90
My Name <me@gm.com>
2026-05-17 13:04:44 +0200

    event_listeners : implement counter incrementation usin v-on event listener directive

--- src/components/EventListener.vue
+++ src/components/EventListener.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>Event Listener</h1>
-</template>   
+    <p>Counter value : {{ count }}</p>
+    <button v-on:click="increment">Increment</button>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const count = ref(0);
+
+    const increment = () => {
+        count.value++;
+    };
+</script>
