c9ff7f3f1ef58e1100aaf2b4ba974e70d346565f
My Name <me@gm.com>
2026-05-17 12:04:38 +0200

    attribute_binding : use shorthand syntax of v-bind attribute binding

--- src/components/AttributeBinding.vue
+++ src/components/AttributeBinding.vue
@@ -1,13 +1,32 @@
 <template>
     <h1 v-bind:class="titleClass">Attribute Binding</h1>
+    <h2 :class="definitionClass">Attribute Binding Definition</h2>
+    <p :id="desc">
+        Attribute binding is a feature in Vue.js that allows you to bind HTML attributes to data properties in your Vue components. 
+        This means that you can dynamically set the value of an attribute based on the state of your component, 
+        making it easier to create interactive and dynamic user interfaces.
+    </p>
+    <dfn :id="def">
+        A common need for data binding is manipulating an element's class list and inline styles. 
+        Since class and style are both attributes, we can use v-bind to assign them a string value dynamically, 
+        much like with other attributes. 
+        However, trying to generate those values using string concatenation can be annoying and error-prone. 
+        For this reason, Vue provides special enhancements when v-bind is used with class and style. 
+        In addition to strings, the expressions can also evaluate to objects or arrays.
+    </dfn>
 </template>
 
 <script setup>
     import { ref } from 'vue';
 
     const titleClass = ref('title');
+    const definitionClass = ref('definition');
+    const desc = ref('description');
+    const def = ref('definition');
 
     console.log("Attribute h1 class bound : "+titleClass.value);
+    console.log("Attribute p id bound : "+desc.value);
+    console.log("Attribute dfn id bound : "+def.value);
 
 </script>
 
@@ -16,4 +35,19 @@
         color: red;
         font-size: 2rem;
     }
+    .definition {
+        color: blue;
+        font-size: 1.5rem;
+    }
+    #description {
+        max-width: 500px;
+        margin: 2rem 0;
+        padding: 1rem 2rem;
+    }
+    #definition{
+        display: inline-block;
+        padding: 1rem 2rem;
+        width: 500px;
+    }
+
 </style>
