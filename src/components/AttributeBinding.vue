e640be74fec0d8c106ad24a4fd8020c6417d79f4
My Name <me@gm.com>
2026-05-17 11:44:37 +0200

    attribute_binding : allocate titleClass variable to the class name 'title' to apply to h1 using v-bind directive

--- src/components/AttributeBinding.vue
+++ src/components/AttributeBinding.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Attribute Binding</h1>
+    <h1 v-bind:class="titleClass">Attribute Binding</h1>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const titleClass = ref('title');
+
+    console.log("Attribute h1 class bound : "+titleClass.value);
+
+</script>
+
+<style>
+    .title {
+        color: red;
+        font-size: 2rem;
+    }
+</style>
