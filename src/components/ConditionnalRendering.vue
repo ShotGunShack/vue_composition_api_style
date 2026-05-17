3c0cdc3c12d9b94a649d55f34b4e41afdc0b2b80
My Name <me@gm.com>
2026-05-17 17:41:32 +0200

    conditionnal_rendering : implement a conditionnal message out of the state of isVisible variable using v-if | v-else directives

--- src/components/ConditionnalRendering.vue
+++ src/components/ConditionnalRendering.vue
@@ -1,3 +1,14 @@
 <template>
     <h1>Conditionnal Rendering</h1>
+    <button @click="isVisible = !isVisible">
+        {{ isVisible ? 'Hide Awesome' : 'Show Awesome' }} Content
+    </button>
+    <p v-if="isVisible">Vue is awesome!</p>
+    <p v-else>Oh no 😢</p>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const isVisible = ref(true);
+</script>
