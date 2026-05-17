2983a0e8091e9992d808c15cca21fc8d0b392c54
My Name <me@gm.com>
2026-05-17 13:14:51 +0200

    event_listeners : implment a decrement method called through shorthand v-on directive version + create a style allocated to this component only using 'scoped' + create some styling for the button using v-bind attribute binding directive

--- src/components/EventListener.vue
+++ src/components/EventListener.vue
@@ -1,15 +1,32 @@
 <template>
     <h1>Event Listener</h1>
     <p>Counter value : {{ count }}</p>
-    <button v-on:click="increment">Increment</button>
+    <button v-on:click="increment" :class="buttonClass">Increment</button>
+    <button @click="decrement" :class="buttonClass">Decrement</button>
 </template>
 
 <script setup>
     import { ref } from 'vue';
 
     const count = ref(0);
+    const buttonClass = ref('button');
 
     const increment = () => {
         count.value++;
     };
+
+    function decrement() {
+        count.value--;
+    }
 </script>
+
+<style scoped>
+
+    .button {
+        padding: 0.5rem 1rem;
+        margin: 0.5rem;
+        font-size: 1rem;
+        cursor: pointer;
+    }
+
+</style>
