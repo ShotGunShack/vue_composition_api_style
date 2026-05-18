0f5efcfcbcce9bd6d21f54d87b5a0e96d066d404
My Name <me@gm.com>
2026-05-18 09:48:27 +0200

    lifecycle_template_refs : amend the content through the use of the onMounted method

--- src/components/LifeCycleTemplateRef.vue
+++ src/components/LifeCycleTemplateRef.vue
@@ -1,3 +1,20 @@
 <template>
     <h1>Lifecycle Template Rendering</h1>
+    <p ref="pElmtRef">Template rendering</p>
+
 </template>
+
+<script setup>
+
+    import { ref, onMounted } from 'vue';
+
+    const pElmtRef = ref(null);
+
+    onMounted(() => {
+        setTimeout(() => {
+            console.log('pElmtRef : ', pElmtRef.value);
+            pElmtRef.value.textContent = "Template ref rendered through onMounted method";
+        }, 5000);
+    });
+
+</script>
