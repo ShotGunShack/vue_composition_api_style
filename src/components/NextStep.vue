0f72a83c2ceb51b496dc368fd3c5e8c6eb23984e
My Name <me@gm.com>
2026-05-20 16:08:46 +0200

    next_step : add js-confetti dependency

--- src/components/NextStep.vue
+++ src/components/NextStep.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>Next Step</h1>
+    <h1 @click="showConfetti">🎉 Congratulations!</h1>
+    <h2>Next Step</h2>
+
 </template>
+
+<script setup>
+    // execute  npm install js-confetti to import it like that
+    // import JSConfetti from 'js-confetti';
+    //Alternative filthy solution  
+    // import JSConfetti from 'https://cdn.jsdelivr.net/npm/js-confetti/+esm'
+
+    const confetti = new JSConfetti();
+
+    function showConfetti() {
+        confetti.addConfetti()
+    }
+
+    showConfetti()
+</script>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        cursor: pointer;
+        margin-top: 3em;
+    }
+</style>
